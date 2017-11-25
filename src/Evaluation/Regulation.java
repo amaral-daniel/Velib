2d8302f8bc43116bf0683e76c754269a24c34799
Julius <Julius@10.30.216.193>
2017-11-25 17:03:47 +0100

    class Regulation commit test

--- src/Evaluation/Regulation.java
+++ src/Evaluation/Regulation.java
@@ -1,11 +1,13 @@
 package Evaluation;
 
-import com.modeliosoft.modelio.javadesigner.annotations.objid;
+// import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
-@objid ("4405897e-9ae0-4032-a51f-ca9500a53a88")
+// @objid ("4405897e-9ae0-4032-a51f-ca9500a53a88")
 public class Regulation {
-    @objid ("54816119-b5c9-409b-9c78-093d919120ee")
+//    @objid ("54816119-b5c9-409b-9c78-093d919120ee")
+    
     public void identifyCriticalStations() {
+    	ArrayList<int>test=new Arraylis<int>();
     }
 
 }
