ea211fcd850c74989c28c209010c35103f5ce0e9
amaral-daniel <danielamaral@poli.ufrj.br>
2017-12-08 15:02:54 +0100

    updates Regulation

--- src/Evaluation/Regulation.java
+++ src/Evaluation/Regulation.java
@@ -4,7 +4,10 @@ package Evaluation;
 
 // @objid ("4405897e-9ae0-4032-a51f-ca9500a53a88")
 
-import Ihm;
+import IHM.*;
+import Data.*;
+import Simulation.*;
+import java.util.ArrayList;
 
 public class Regulation {
 //    @objid ("54816119-b5c9-409b-9c78-093d919120ee")
@@ -12,15 +15,18 @@ public class Regulation {
 	/** The Method evaluates for a given Scenario the critical States of all Stations and writes it to file **/
     public void identifyCriticalStations(Scenario refScenario) {
     	
+    	ArrayList<Station> stationList = refScenario.getStations();
     	// loop running through all Stations of the given Scenario
-    	for (int i, i < refScenario.stationList.size(),i++) {
+    	for (int i; i < stationList.size();i++) {
     		
-    		Station currentStation = refScenario.stationList.get(i);
+    		Station currentStation = stationList.get(i);
+    		ArrayList<State> stationStates = currentStation.getStates();
+    		if
     		
     		//loop running through all States j of the selected Station i
-    		for	(int j, j < currentStation.stateList.size(), j++) {
+    		for	(int j; j < stationStates.size(); j++) {
     			
-    			State currentState = currentStation.stateList.get(j);
+    			State currentState = stationStates.get(j);
     					
     			//check if the station i is empty in State j
     			if (currentState.isEmpty() || currentState.isFull()) {
