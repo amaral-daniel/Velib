39f5eb9a2ff8792e9b1fc51dbf12aa474af140ed
matorsoni <matorsoni45@gmail.com>
2017-12-09 16:21:03 +0100

    Added file reader and writer

--- src/IHM/Read.java
+++ src/IHM/Read.java
@@ -1,18 +1,100 @@
+//package IHM;
+
+//import com.modeliosoft.modelio.javadesigner.annotations.objid;
+
+//@objid ("13ef917a-3abc-45ed-bde2-178f34720905")
+//public class Read {
+//    @objid ("58175a03-1e5d-418d-961c-1e04642a1e43")
+//    public void loadTrips() {
+//    }
+
+//    @objid ("9e38d89f-aca5-49e2-9182-8f1ecba2a130")
+//    public void loadStations() {
+//    }
+
+//    @objid ("73907320-92d8-485f-bc54-280a64d2791a")
+//    public void loadState() {
+//  }
+//}
+
 package IHM;
 
-import com.modeliosoft.modelio.javadesigner.annotations.objid;
+import java.io.*;
 
-@objid ("13ef917a-3abc-45ed-bde2-178f34720905")
-public class Read {
-    @objid ("58175a03-1e5d-418d-961c-1e04642a1e43")
-    public void loadTrips() {
-    }
+// 
+public class Read 
+{
+	/* Class Read:
+	 * Attributes: 
+	 * 2 Strings representing the path of address file and trips file.
+	 * Format: "Velib\\src\\files\\filename.txt"
+	 * 
+	 * Methods: 
+	 * readTrips: REads trips file and converts the text in data.
+	 * 
+	 * */
+	private String addressesFileName;
+	private String tripsFileName;
+	
+	public Read (String addressesFileName, String tripsFileName)
+	{	
+		this.addressesFileName = addressesFileName;
+		this.tripsFileName = tripsFileName;
+	}
+	
+	
+	public void readAddresses(String addressesFileName)
+	{
+		
+	}
+	
+	public void readTrips(String tripsFileName)
+	{	
+		File tripsFile = new File(tripsFileName);
+		
+		String line = null;
+		
+		try 
+		{
+			FileReader fr = new FileReader(tripsFile);
+		
+			BufferedReader buffer = new BufferedReader(fr);
+		
+			while((line = buffer.readLine())!= null) 
+			{
+				//here the data will be stored in the
+				//different objects of the project
+				System.out.println(line);
+			}
+			buffer.close();
+		}
+		catch(FileNotFoundException error) 
+		{
+			System.out.println("File not found");
+		}
+		catch(IOException error) 
+		{
+			System.out.println("Something went wrong");
+		}
+	}
+	
+	
+	
+	
+	public static void main (String[] args) 
+	{
+		String tripsFileName = "Velib\\src\\files\\trips-2013-10-31.txt";
+		String addressesFileName = "Velib\\src\\files\\stationAddresses.txt";
+		String test = "Velib\\src\\files\\writing_test.txt";
+		
+		
+		Read read = new Read(addressesFileName, tripsFileName);
+		
+		read.readTrips(test);
+		
+		
+	}
+	
+	
 
-    @objid ("9e38d89f-aca5-49e2-9182-8f1ecba2a130")
-    public void loadStations() {
-    }
-
-    @objid ("73907320-92d8-485f-bc54-280a64d2791a")
-    public void loadState() {
-    }
 }
--- src/IHM/Write.java
+++ src/IHM/Write.java
@@ -1,23 +1,71 @@
+//package IHM;
+
+//import Simulation.Scenario;
+//import com.modeliosoft.modelio.javadesigner.annotations.objid;
+
+//@objid ("8208822d-876b-49c2-bbe2-a528d90560a8")
+//public class Write {
+//    @objid ("9f965698-b7b0-4aeb-a14c-ff03447d579e")
+ //   public class Interface {
+   //     @objid ("6d54e82e-a53d-432e-939d-a9beeec50b3a")
+//        public Scenario scenario;
+
+//        @objid ("b5207468-39be-4e81-8c7d-53b5832cbf1b")
+//        public void displayScenario() {
+//        }
+
+//        @objid ("1d07c974-ae11-4979-8f2a-6ea7a6315eb3")
+//        public void askUser(final int simulationCase) {
+//        }
+
+//    }
+
+//}
+
 package IHM;
 
-import Simulation.Scenario;
-import com.modeliosoft.modelio.javadesigner.annotations.objid;
+import java.io.*;
 
-@objid ("8208822d-876b-49c2-bbe2-a528d90560a8")
-public class Write {
-    @objid ("9f965698-b7b0-4aeb-a14c-ff03447d579e")
-    public class Interface {
-        @objid ("6d54e82e-a53d-432e-939d-a9beeec50b3a")
-        public Scenario scenario;
-
-        @objid ("b5207468-39be-4e81-8c7d-53b5832cbf1b")
-        public void displayScenario() {
-        }
-
-        @objid ("1d07c974-ae11-4979-8f2a-6ea7a6315eb3")
-        public void askUser(final int simulationCase) {
-        }
-
-    }
-
+public class Write 
+{
+	private String fileName;
+	
+	
+	public Write (String fileName)
+	{
+		this.fileName = fileName;
+	}
+	
+	
+	public void writeFile()
+	{
+		
+		try 
+		{
+			FileWriter fileWriter = new FileWriter(this.fileName);
+		
+			BufferedWriter wBuffer = new BufferedWriter(fileWriter);
+			
+			
+			
+			wBuffer.write("valeu cuzao");
+			wBuffer.newLine();
+			wBuffer.write("flws");
+			
+			wBuffer.close();
+		}
+		catch(IOException error) 
+		{
+			System.out.println("Something went wrong while writing");
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		String fileName = "Velib\\src\\files\\wrinting_test.txt";
+		Write escritura = new Write(fileName);
+		escritura.writeFile();
+		//bla
+		
+	}
 }
