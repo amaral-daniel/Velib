336b0cbb7f1a3d00004ee729f3ccc22d93e1d620
amaral-daniel <danielamaral@poli.ufrj.br>
2017-12-09 15:20:50 +0100

    Adds EvaluateurStation and EvaluateurScenario

--- src/Evaluation/EvaluateurStation.java
+++ src/Evaluation/EvaluateurStation.java
@@ -1,5 +1,49 @@
 package Evaluation;
+import java.util.ArrayList;
 
-public class EvaluateurStation {
+import Data.*;
 
+public final class EvaluateurStation {
+	private EvaluateurStation()
+	{
+		minimalCriticalTime = 30*60; //30 minutes 
+	}
+	
+	private static int minimalCriticalTime;
+	
+	public static void setMinimalCriticalTime(int n)
+	{
+		minimalCriticalTime = n;
+	}
+	
+	
+	public static boolean isCritical(Station stationAEvaluer)
+	{
+
+		ArrayList<State> stationStates = stationAEvaluer.getStates();
+		int criticalTime = 0;
+		
+		//loop running through all States j of the selected Station i
+		for	(int j; j < stationStates.size() - 1; j++) {
+			
+			State currentState = stationStates.get(j);
+					
+			//check if the station i is almost empty or full in State j
+			if (currentState.getBikes() == < 2 || currentState.getBikes() == currentStation.getCapacity()) 
+			{			
+				State nextState = stationStates.get(j + 1);
+				int durationState = nextState.getTime() - currentState.getTime() ;	
+				criticalTime += durationState;
+			}
+					
+		}			
+		
+		if(criticalTime >= minimalCriticalTime)
+		{
+			return true;
+		}
+		return false;
+	}
+	
+	
 }
